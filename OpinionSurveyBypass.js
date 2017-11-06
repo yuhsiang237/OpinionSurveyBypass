7247c72b61ba39bd5a293f510502f0f9762d9a93
UM <umworksite@gmail.com>
2017-11-05 23:05:22 -0600

    Update OpinionSurveyBypass.js

--- OpinionSurveyBypass.js
+++ OpinionSurveyBypass.js
@@ -1,1 +1,17 @@
-javascript:(function () {var f1 = window.frames[0];f2 = f1.window.frames[1];f3 = f2.frames[1];var text = f3.document.getElementsByTagName("body")[0].innerHTML;var regex = /id.*?(rdBtn.*?Y)"/g;while ((m = regex.exec(text)) !== null ) {console.log(m[1]);f3.document.getElementById(m[1]).checked = true;}f3.document.getElementById("txtAnsLike").value = "非常棒!";f3.document.getElementById('btnIns').click();})();
+javascript:(function () {
+
+  var f1 = window.frames[0];
+  f2 = f1.window.frames[1];
+  f3 = f2.frames[1];
+
+  var text = f3.document.getElementsByTagName("body")[0].innerHTML;
+  var regex = /id.*?(rdBtn.*?Y)"/g;
+  
+  while ((m = regex.exec(text)) !== null ) {
+    f3.document.getElementById(m[1]).checked = true;
+  }
+  
+  f3.document.getElementById("txtAnsLike").value = "非常棒!";
+  f3.document.getElementById('btnIns').click();
+
+})();
